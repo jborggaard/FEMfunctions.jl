72f6fda04882ea82791545c45354154dbb6e1d6d
Jeff Borggaard <jborggaard@vt.edu>
2020-12-14 20:38:08 -0500

    forcing function fix

--- test/twodStokes.jl
+++ test/twodStokes.jl
@@ -16,31 +16,33 @@ function twodStokes(N::Int)
 #  @everywhere μ = 0.001;
 #  @everywhere ϵ = 0.0;
   μ = 0.01;
-  ϵ = 0.00005;
+  ϵ = 0;      # 1e-5;
 
-  xMin =-1.0;
-  xMax = 1.0;
-  yMin =-1.0;
-  yMax = 1.0;
+  xMin = -1.0;
+  xMax =  1.0;
+  yMin = -1.0;
+  yMax =  1.0;
 
-  rule  = 7;   # points in quadrature formula
+  rule = 7;   # points in quadrature formula
 
 
   #  Specify manufactured solutions for code verification
   #-----------------------------------------------------------------------------
-  function zExact(x)
-     return -x[2]*(1-x[2]^2)
-#     return  x[1]*(1-x[1]^2)
-#    return sin(π*x[1])*sin(2.0*π*x[2]);
+  function uExact(x)
+    return -x[2]*(1-x[2]^2)
+  end
+
+  function vExact(x)
+    return  x[1]*(1-x[1]^2)
   end
 
 #  @everywhere function fx(x::Array{Float64,2})
   function fx(x::Array{Float64,2})
-    return 1.0-6.0*μ*x[2]
+    return 1.0-6.0*μ*x[1,2]
   end
 #  @everywhere function fy(x::Array{Float64,2})
   function fy(x::Array{Float64,2})
-    return 1.0+6.0*μ*x[1]
+    return 1.0+6.0*μ*x[1,1]
   end
 
 
@@ -57,16 +59,14 @@ function twodStokes(N::Int)
   #-----------------------------------------------------------------------------
   ideU = zeros(Int64,nNodes,2); 
 
+  # easily streamlined... since boundaries are handled later
   global nUnk = 0;
   for i=1:nNodes
-    if ideU[i,1]==0
-      global nUnk = nUnk+1;
-      ideU[i,1] = nUnk;
-    end
-    if ideU[i,2]==0
-      global nUnk = nUnk+1;
-      ideU[i,2] = nUnk;
-    end
+    global nUnk = nUnk+1;
+    ideU[i,1] = nUnk;
+
+    global nUnk = nUnk+1;
+    ideU[i,2] = nUnk;
   end
 
   ideP = zeros(Int64,nNodes);
@@ -107,15 +107,15 @@ function twodStokes(N::Int)
   II  = Array{Int64,1}(undef, nEntries);
   JJ  = Array{Int64,1}(undef, nEntries);
   AA  = Array{Float64,1}(undef, nEntries);
-  b   = zeros(Float64,2*nNodes,1);
+  b   = zeros(Float64,nUnk,1);
 
 #  @sync @distributed for k=1:nElements
   for k=1:nElements
-    xg  = zeros(Float64,rule,2);
-    wg  = zeros(Float64,rule);
-    phi = zeros(Float64,rule,nElDOF);
-    p_x = zeros(Float64,rule,nElDOF);
-    p_y = zeros(Float64,rule,nElDOF);
+#    xg  = zeros(Float64,rule,2);
+#    wg  = zeros(Float64,rule);
+#    phi = zeros(Float64,rule,nElDOF);
+#    p_x = zeros(Float64,rule,nElDOF);
+#    p_y = zeros(Float64,rule,nElDOF);
 
     nLocal = eConn[k,:][:];
     xLocal = x[nLocal,:];
@@ -123,8 +123,10 @@ function twodStokes(N::Int)
     xg,wg,phi,phi_x,phi_y = twodShape( xLocal, r, s, w );
     xg,wg,psi,psi_x,psi_y = twodShape( xLocal[1:3,:], r, s, w );
 
-    fx_g   = fx(xg);           # forcing function evaluated at quadrature points
-    fy_g   = fy(xg);           # forcing function evaluated at quadrature points
+    
+    # evaluate forcing function at quadrature points
+    fx_g = fx(xg)
+    fy_g = fy(xg)
 
     A11Loc =-twodBilinear( 2*μ_g, phi_x, phi_x, wg) -
              twodBilinear(   μ_g, phi_y, phi_y, wg) ;
@@ -224,7 +226,7 @@ function twodStokes(N::Int)
         index = index + 1;
         II[index] = nTest;
         JJ[index] = nUnkP;
-        AA[index] = MLoc[np,nt];
+        AA[index] = MLoc[nt,np];
       end
 
     end
@@ -237,14 +239,15 @@ function twodStokes(N::Int)
   #-----------------------------------------------------------------------------
   nDirichlet = size(iB,1);
 
+  sort!(iB)
   knownIndexU = [2*iB-ones(Int64,nDirichlet,1)]
   knownIndexV = [2*iB]
 
   dirichletU = zeros(Float64,nDirichlet,1)
   dirichletV = zeros(Float64,nDirichlet,1)
   for i = 1:length(iB)
-    dirichletU[i] =-x[iB[i],2]*(1.0-x[iB[i],2]^2)
-    dirichletV[i] = x[iB[i],1]*(1.0-x[iB[i],1]^2)
+    dirichletU[i] = uExact(x[iB[i],:])
+    dirichletV[i] = vExact(x[iB[i],:])
   end   
   
   interiorNodes = 1:nNodes;
@@ -253,26 +256,32 @@ function twodStokes(N::Int)
 
   nUnknowns = 2*length(interiorNodes) + nP;
   unknownIndex = Array{Int64,1}(undef,nUnknowns)
-  uIndex = Array{Int64,1}(undef,length(interiorNodes))
-  vIndex = Array{Int64,1}(undef,length(interiorNodes))
+  uIndex = Array{Int64,1}(undef,2*length(interiorNodes))
+  vIndex = Array{Int64,1}(undef,2*length(interiorNodes))
   uInverseIndex = Array{Int64,1}(undef,nNodes)
   vInverseIndex = Array{Int64,1}(undef,nNodes)
 
   global nUnknownCounter = 0;
   for i ∈ interiorNodes
-    global nUnknownCounter = nUnknownCounter+1;
-    unknownIndex[nUnknownCounter] = 2*i-1;
-    uIndex[nUnknownCounter] = i;
-    uInverseIndex[i]=nUnknownCounter;
+    global nUnknownCounter = nUnknownCounter+1
+    unknownIndex[nUnknownCounter] = 2*i-1
+    uIndex[nUnknownCounter] = i
+    uInverseIndex[i]=nUnknownCounter
 
-    global nUnknownCounter = nUnknownCounter+1;
-    unknownIndex[nUnknownCounter] = 2*i;
-    vIndex[nUnknownCounter] = i;
-    vInverseIndex[i]=nUnknownCounter;
+    global nUnknownCounter = nUnknownCounter+1
+    unknownIndex[nUnknownCounter] = 2*i
+    vIndex[nUnknownCounter] = i
+    vInverseIndex[i]=nUnknownCounter
   end
 
-#  unknownIndex = 1:nUnknowns
-  rhs = b[unknownIndex]-A[unknownIndex,knownIndexU[][:]]*dirichletU-A[unknownIndex,knownIndexV[][:]]*dirichletV
+  for nNode=1:nNodes
+    if ideP[nNode]>0
+      global nUnknownCounter = nUnknownCounter + 1
+      unknownIndex[nUnknownCounter] = ideP[nNode]
+    end
+  end
+
+  rhs = -b[unknownIndex]-A[unknownIndex,knownIndexU[][:]]*dirichletU-A[unknownIndex,knownIndexV[][:]]*dirichletV
 
   uvp = A[unknownIndex,unknownIndex]\rhs
 
@@ -287,21 +296,22 @@ function twodStokes(N::Int)
     velocity[i,2] = uvp[nUnknownCounter];
   end
 
-#  for i=1:nDirichlet
-#    velocity[iB[i],1] = dirichletU[i];
-#    velocity[iB[i],2] = dirichletV[i];
-#  end
-  @printf("nUnknowns %g\n",nUnknowns)
-  @printf("nUnknownCounter %g\n",nUnknownCounter)
+  for i=1:nDirichlet
+    velocity[iB[i],1] = dirichletU[i];
+    velocity[iB[i],2] = dirichletV[i];
+  end
+#  @printf("nUnknowns %g\n",nUnknowns)
+#  @printf("nUnknownCounter %g\n",nUnknownCounter)
 
 
   #  Compute the exact solution for the unit test
-  uExact = zeros(Float64,nNodes,1);
+  velocityExact = zeros(Float64,nNodes,2);
   for n=1:nNodes
-    uExact[n] = zExact(x[n,:])
+    velocityExact[n,1] = uExact(x[n,:])
+    velocityExact[n,2] = vExact(x[n,:])
   end
 
-  return A,b,rhs,knownIndexU,knownIndexV,unknownIndex,dirichletU,dirichletV
-#  return x,eConn,velocity,uExact
-#  return norm(velocity[:,1]-uExact) #A,b
+#  return A,b,rhs,knownIndexU,knownIndexV,unknownIndex,dirichletU,dirichletV
+#  return x,eConn,velocity,velocityExact
+  return norm(velocity-velocityExact) # proxy for sqrt((v-vE)'*M*(v-vE))
 end
